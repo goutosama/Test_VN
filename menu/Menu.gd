05b7f3c68624ff6b5d643e52428b05066781807a
fagirton <fagirton2015@gmail.com>
2023-10-12 23:04:14 +0300

    Pre-hackaton commit

--- menu/Menu.gd
+++ menu/Menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-onready var SelectedItem = 1;
+onready var SelectedItem = 0;
 onready var Items : Array = [
 	"Continue",
 	"Quest List",
@@ -13,51 +13,82 @@ onready var Items : Array = [
 onready var counter : int = 0;
 onready var animSpeed : int = 80/5;
 onready var isCursorAnims : bool = false
+onready var isSelectAnim : bool = false
+onready var isSelected : bool = false
 
-onready var cursor = get_node("Cursor")
-onready var cursorLabel = get_node("Cursor/MarginContainer/Label")
-onready var ItemsContainer = get_node("PanelContainer/CanvasLayer/VBoxContainer")
+onready var Cursor = get_node("Cursor")
+onready var CursorLabel = get_node("Cursor/MarginContainer/Label")
+onready var ItemsContainer = get_node("VerticalSlider/CanvasLayer/VBoxContainer")
+onready var Vslider = get_node("VerticalSlider")
+onready var InfoPanel = get_node("MarginContainer/PanelContainer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	CursorLabel.text = Items[SelectedItem]
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-
-#	if cursor.rect_position.y < 12 :
-#		SelectedItem = 0
-#		cursor.rect_position.y = 12
-#	if cursor.rect_position.y > 12 + 82 * ItemsContainer.get_child_count() :
-#		SelectedItem = ItemsContainer.get_child_count()
-#		cursor.rect_position.y = 12 + 82 * ItemsContainer.get_child_count()
-	
+	# animation for cursor change
 	if counter != 0:
-		print(counter, "  ",  cursor.rect_position.y, " ", SelectedItem)
-		if counter > 0:
-			counter -= animSpeed
-			cursor.rect_position.y -= animSpeed
-		if counter < 0:
-			counter += animSpeed
-			cursor.rect_position.y += animSpeed
+		if isSelectAnim:
+			if counter > 0:
+				counter -= animSpeed
+				Cursor.rect_position.y -= animSpeed
+				Vslider.rect_position.y -= animSpeed
+				ItemsContainer.rect_position.y -= animSpeed
+				if counter == animSpeed:
+					InfoPanel.visible = true
+					$AnimationPlayer.play("ShowInfo")
+			if counter < 0:
+				counter += animSpeed
+				Cursor.rect_position.y += animSpeed
+				Vslider.rect_position.y += animSpeed
+				ItemsContainer.rect_position.y += animSpeed
+
+		else:
+			if counter > 0:
+				counter -= animSpeed
+				Cursor.rect_position.y -= animSpeed
+			if counter < 0:
+				counter += animSpeed
+				Cursor.rect_position.y += animSpeed
+
 	if counter == 0:
 		isCursorAnims = false
+		isSelectAnim = false
 	
 	if Input.is_action_just_pressed("ui_left"):
-		pass
+		if !isCursorAnims && isSelected && SelectedItem != 0:
+			isCursorAnims = true
+			isSelectAnim = true
+			isSelected = false
+			$AnimationPlayer.play("HideInfo")
+			
 	if Input.is_action_just_pressed("ui_right"):
-		pass
+		if !isCursorAnims && !isSelected:
+			isCursorAnims = true
+			isSelectAnim = true
+			isSelected = true
+			counter = 80 * SelectedItem
 	if Input.is_action_just_pressed("ui_up"):
-		if SelectedItem != 0 && !isCursorAnims:
+		if SelectedItem != 0 && !isCursorAnims && !isSelected:
 			SelectedItem -= 1
 			isCursorAnims = true
 			counter = 80
-			cursorLabel.text = Items[SelectedItem]
+			CursorLabel.text = Items[SelectedItem]
 	if Input.is_action_just_pressed("ui_down"):
-		if SelectedItem != ItemsContainer.get_child_count() - 1 && !isCursorAnims:
-			cursorLabel.text = Items[SelectedItem]
+		if SelectedItem != ItemsContainer.get_child_count() - 1 && !isCursorAnims && !isSelected:
 			SelectedItem += 1
 			isCursorAnims = true
 			counter = -80
-			cursorLabel.text = Items[SelectedItem]
+			CursorLabel.text = Items[SelectedItem]
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "HideInfo":
+		isCursorAnims = true
+		isSelectAnim = true
+		isSelected = false
+		counter = -80 * SelectedItem
